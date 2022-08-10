1db945aac0ed673160dbeb263f7f04e09f1bd90d
yara-amr <100420875+yara-amr@users.noreply.github.com>
2022-08-10 18:56:49 +0200

    Update createDB.py

--- createDB.py
+++ createDB.py
@@ -1,4 +1,4 @@
-import db
+ import db
 import sqlite3
 from msilib.schema import tables
 from db import get_db, close_db 
@@ -12,17 +12,15 @@ def create_users_tables():
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL,
             email TEXT NOT NULL,
-            password TEXT NOT NULL,
-            comment TEXT,
-            favorite_movie Text
+            password TEXT NOT NULL
             );""")
     except Exception as err:
         print("Table is Created Before", str(err))
 
     connect_db.commit()
     close_db(connect_db) 
-
 create_users_tables()           
+
 
 def create_movies_table():
     connect_db = get_db()
@@ -36,59 +34,23 @@ def create_movies_table():
             name TEXT NOT NULL,
             release_year TEXT NOT NULL,
             details TEXT,
-            photo BLOB NOT NULL,
-            comment TEXT);""")
+            photo BLOB NOT NULL);""")
 
     except Exception as err:
         print("Table is Created Before", str(err))
 
     connect_db.commit()
     close_db(connect_db) 
-
 create_movies_table() 
 
 
 def convertToBinaryData(filename):
 	
-	# Convert binary format to images
-	# or files data
+	# Convert binary format to images or files data
 	with open(filename, 'rb') as file:
 		blobData = file.read()
 	return blobData
 
-
-def createMoviesDataTable():
-	try:
-		
-		# Using connect method for establishing
-		# a connection
-		sqliteConnection = sqlite3.connect('new_employee.db')
-		cursor = sqliteConnection.cursor()
-		print("Connected to SQLite")
-		
-		# insert query
-		sqlite_Create_blob_query = """ CREATE TABLE movies_data (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL,
-            release_year TEXT NOT NULL,
-            details TEXT,
-            photo	BLOB NOT NULL);"""
-		
-		# using cursor object executing our query
-		cursor.execute(sqlite_Create_blob_query)
-		sqliteConnection.commit()
-		print("Image and file create successfully as a BLOB into a table")
-		cursor.close()
-
-	except sqlite3.Error as error:
-		print("Failed to create blob data into sqlite table", error)
-	
-	finally:
-		if sqliteConnection:
-			sqliteConnection.close()
-			print("the sqlite connection is closed")
-
-createMoviesDataTable()
 
 def insertIntoDB(name, release_year, details, photo):
     connect_db = get_db()
@@ -110,29 +72,42 @@ def insertIntoDB(name, release_year, details, photo):
         print("Failed to insert blob data into sqlite table", str(err))
     
 
-def insertBLOB(name, release_year, details, photo):
-    try:
-        sqliteConnection = sqlite3.connect('new_employee.db')
-        cursor = sqliteConnection.cursor()
-        print("Connected to SQLite")
-        sqlite_insert_blob_query = """ INSERT INTO movies_data
-                                  (name, release_year, details, photo) VALUES (?, ?, ?, ?)"""
+def create_comment_tables():
+    connect_db = get_db()
 
-        empPhoto = convertToBinaryData(photo)
-        # Convert data into tuple format
-        data_tuple = (name, release_year, details, empPhoto)
-        cursor.execute(sqlite_insert_blob_query, data_tuple)
-        sqliteConnection.commit()
-        print("Image and file inserted successfully as a BLOB into a table")
-        cursor.close()
+    try: 
+        #Create Table
+        connect_db.execute("""CREATE TABLE comment (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            movie_name TEXT NOT NULL,
+            user_email TEXT NOT NULL,
+            comment_text TEXT 
+            );""")
+    except Exception as err:
+        print("Table is Created Before", str(err))
 
-    except sqlite3.Error as error:
-        print("Failed to insert blob data into sqlite table", error)
-    finally:
-        if sqliteConnection:
-            sqliteConnection.close()
-            print("the sqlite connection is closed")
+    connect_db.commit()
+    close_db(connect_db) 
+create_comment_tables()   
 
+
+def create_favorite_tables():
+    connect_db = get_db()
+
+    try: 
+        #Create Table
+        connect_db.execute("""CREATE TABLE favorite (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            movie_name TEXT NOT NULL,
+            user_email TEXT NOT NULL);""")
+    except Exception as err:
+        print("Table is Created Before", str(err))
+
+    connect_db.commit()
+    close_db(connect_db) 
+create_favorite_tables()
+
+#### Adding Data In movies_data #####
 
 insertIntoDB("Coco", "2021", "Family Movie", "E:\\MoviesWebsite\\task4\\task4\\static\\images\\Coco.jpg")
 insertIntoDB("Harry Potter And The Sorcerer's Stone", "2001", "Family Movie", "E:\\MoviesWebsite\\task4\\task4\\static\\images\\Harry Potter And The Sorcerer's Stone.jpg")
@@ -162,35 +137,3 @@ insertIntoDB("Me Before You", "2016", "Romantic Movie", "E:\\MoviesWebsite\\task
 insertIntoDB("Interstellar", "2001", "Action Movie", "E:\\MoviesWebsite\\task4\\task4\\static\\images\\Interstellar.jpg")
 insertIntoDB("Bring The Soul", "2019", "Romantic Movie", "E:\\MoviesWebsite\\task4\\task4\\static\\images\\Bring The Soul.jpg")
 insertIntoDB("Onward", "2021", "Family Movie", "E:\\MoviesWebsite\\task4\\task4\\static\\images\\Onward.jpg")
-
-
-
-
-insertBLOB("Coco", "2021", "Family Movie", "E:\\MoviesWebsite\\task4\\task4\\static\\images\\Coco.jpg")
-insertBLOB("Harry Potter And The Sorcerer's Stone", "2001", "Family Movie", "E:\\MoviesWebsite\\task4\\task4\\static\\images\\Harry Potter And The Sorcerer's Stone.jpg")
-insertBLOB("Spider-Man No Way Home", "2021", "Action Movie", "E:\\MoviesWebsite\\task4\\task4\\static\\images\\Spider-Man No Way Home.jpg")
-insertBLOB("The Hunger Games", "2016", "Action Movie", "E:\\MoviesWebsite\\task4\\task4\\static\\images\\The Hunger Games.jpg")
-insertBLOB("Jumanji", "2021", "Action Movie", "E:\\MoviesWebsite\\task4\\task4\\static\\images\\Jumanji.jpg")
-insertBLOB("The Maze Runner", "2011", "Action Movie", "E:\\MoviesWebsite\\task4\\task4\\static\\images\\The Maze Runner.jpg")
-insertBLOB("Lightning Ship", "2021", "Action Movie", "E:\\MoviesWebsite\\task4\\task4\\static\\images\\Lightning Ship.jpg")
-insertBLOB("The Pirates The Last Royal Treasure", "2022", "Action Movie", "E:\\MoviesWebsite\\task4\\task4\\static\\images\\The Pirates The Last Royal Treasure.jpg")
-insertBLOB("Pipeline", "2021", "Action Movie", "E:\\MoviesWebsite\\task4\\task4\\static\\images\\Pipeline.jpg")
-insertBLOB("Shark The Beginning", "2021", "Action Movie", "E:\\MoviesWebsite\\task4\\task4\\static\\images\\Shark The Beginning.jpg")
-insertBLOB("Happy New Year", "2016", "Comedian Movie", "E:\\MoviesWebsite\\task4\\task4\\static\\images\\Happy New Year.jpg")
-insertBLOB("Soul", "2021", "Family Movie", "E:\\MoviesWebsite\\task4\\task4\\static\\images\\Soul.jpg")
-insertBLOB("Turning Red", "2021", "Family Movie", "E:\\MoviesWebsite\\task4\\task4\\static\\images\\Turning Red.jpg")
-insertBLOB("Sing 2", "2022", "Family Movie", "E:\\MoviesWebsite\\task4\\task4\\static\\images\\Sing 2.jpg")
-insertBLOB("Cars", "2012", "Family Movie", "E:\\MoviesWebsite\\task4\\task4\\static\\images\\Cars.jpg")
-insertBLOB("Encanto", "2022", "Family Movie", "E:\\MoviesWebsite\\task4\\task4\\static\\images\\Encanto.jpg")
-insertBLOB("Train To Busan", "2020", "Horror Movie", "E:\\MoviesWebsite\\task4\\task4\\static\\images\\Train To Busan.jpg")
-insertBLOB("Parasite", "2021", "Horror Movie", "E:\\MoviesWebsite\\task4\\task4\\static\\images\\Parasite.jpg")
-insertBLOB("The Cursed The Dead Man's Prey", "2019", "Horror Movie", "E:\\MoviesWebsite\\task4\\task4\\static\\images\\The Cursed The Dead Man's Prey.jpg")
-insertBLOB("Five Feet Apart", "2018", "Romantic Movie", "E:\\MoviesWebsite\\task4\\task4\\static\\images\\Five Feet Apart.jpg")
-insertBLOB("The Greatest Showman", "2010", "Romantic Movie", "E:\\MoviesWebsite\\task4\\task4\\static\\images\\The Greatest Showman.jpg")
-insertBLOB("The Theory Of Everything", "2017", "Romantic Movie", "E:\\MoviesWebsite\\task4\\task4\\static\\images\\The Theory Of Everything.jpg")
-insertBLOB("Bajirao Mastani", "2015", "Romantic Movie", "E:\\MoviesWebsite\\task4\\task4\\static\\images\\Bajirao Mastani.jpg")
-insertBLOB("Rab Ne Bana Di Jodi", "2018", "Romantic Movie", "E:\\MoviesWebsite\\task4\\task4\\static\\images\\Rab Ne Bana Di Jodi.jpg")
-insertBLOB("Me Before You", "2016", "Romantic Movie", "E:\\MoviesWebsite\\task4\\task4\\static\\images\\Me Before You.jpg")
-insertBLOB("Interstellar", "2001", "Action Movie", "E:\\MoviesWebsite\\task4\\task4\\static\\images\\Interstellar.jpg")
-insertBLOB("Bring The Soul", "2019", "Romantic Movie", "E:\\MoviesWebsite\\task4\\task4\\static\\images\\Bring The Soul.jpg")
-insertBLOB("Onward", "2021", "Family Movie", "E:\\MoviesWebsite\\task4\\task4\\static\\images\\Onward.jpg")
